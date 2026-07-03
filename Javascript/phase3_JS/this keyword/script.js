65867c112c03e296cd430ddd8ef1bbd1c0e3f1db
mansii-05 <singhmanc.335@gmail.com>
2026-07-03 23:43:37 +0530

    Add this keyword in JS

---
+++ Javascript/phase3_JS/this keyword/script.js
@@ -0,0 +1,97 @@
+console.log(this); //refers to window object in global scope
+
+b = 10; //directly making global variable
+
+a = 20; //ignored
+var a = 30;
+console.log(a) //30
+
+//-------------------------'this' in regular functions-----------------------------------------
+
+function show(){
+    console.log(this); // window object, coz this is referring towards global object
+}
+show();
+
+//------------------------------ this in method ------------------------------------------------
+
+var obj2 = {
+    fname: "Abhishek ",
+    lname: "Sharma",
+    age : 20,
+    getIntro: function(){
+        console.log(this.fname + this.lname); //refer to object itself
+    } ,
+    updateAge: function (){
+        console.log(this.age = 25);//update age
+    }
+}
+obj2.getIntro() // refers to object, returns fname and lname of obj2
+obj2.updateAge() //30 --> (25)
+
+//------------------------------method scope of regular function-------------------------------------
+
+var obj = {
+    fname: "John",
+    lname: "Doe",
+    age : 30,
+
+    getIntro: function(){ //method
+        console.log(this); //refer to object itself
+    },
+
+    sayHi : function (){  
+        //function inside method
+        var sayBye = function () {
+                console.log(this); //window object
+        }
+        sayTata = function () {
+                console.log(this); //window object
+        }
+        sayBye()
+        sayTata()
+    }
+}
+
+obj.getIntro() // refers to object
+console.log("method scope of regular function")
+obj.sayHi(); //window object
+
+
+//------------------------------this in FAT Arrow function---------------------------------------------
+
+//it has no lexical context of its own, so it refers to window object in global scope, and to the object in method scope
+console.log("\n")
+console.log("fat arrow function")
+var obj3 = {
+    fname: "Sara ",
+    lname: "Tendulkar",
+    age : 21,
+
+    getIntro: function(){
+        // console.log(this); //refers to window object
+        sayName=  () => {  //inside normal func, refers to the lexical context of the function inside which it is defined.
+        console.log(this.fname + this.lname); //refer to object itself
+        }
+        sayName()
+    },
+
+    fatArrow: () => {  //fat arrow
+        console.log(this) //window object
+    },
+
+    sayHi : () => {
+        sayBye = () => {
+                console.log(this); //window object
+        }
+        sayBye()
+    }
+
+}
+
+obj3.fatArrow(); //window object
+
+obj3.sayHi() //window object
+
+console.log("method scope of arrow func")
+obj3.getIntro()
